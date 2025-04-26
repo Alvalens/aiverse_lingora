7f0fca993be1593c7bcb6aced714c6ecb07fcb09
Alvalens <alvalen.shafel04@gmail.com>
2025-04-26 14:51:15 +0700

    feat: update layout metadata and add Toaster component

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,8 @@ import { NextAuthProvider } from "@/providers/NextAuthProviders";
 import ReactQueryProvider from "@/providers/ReactQueryProvider";
 import Script from "next/script";
 import "./globals.css";
-import ReactQueryProvider from "@/providers/ReactQueryProvider";
+
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,65 +19,13 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: {
-    default: "Intervyou | Platform Wawancara & Analisis CV",
-    template: "%s | Intervyou",
-    absolute: "Intervyou | Platform Wawancara & Analisis CV",
+    default: "Lingora | Platform Belajar Bahasa Inggris",
+    template: "%s | Lingora",
+    absolute: "Lingora",
   },
-  applicationName: "Intervyou",
+  applicationName: "Lingora",
   description:
-    "Platform pelatihan wawancara kerja dan optimasi CV. Dapatkan saran, evaluasi, dan pelatihan untuk mengembangkan karir.",
-  keywords: [
-    "wawancara kerja",
-    "analisis CV",
-    "pengembangan karir",
-    "resume",
-    "lowongan kerja",
-    "platform wawancara",
-    "latihan wawancara",
-    "AI interview",
-    "AI resume",
-    "AI career development",
-    "AI CV analysis",
-  ],
-  authors: [
-    {
-      name: "AI Verse",
-      url: "https://www.intervyou.me",
-    },
-  ],
-  openGraph: {
-    title: "Intervyou | Platform Pelatihan Wawancara AI",
-    description:
-      "Platform inovatif untuk membantu profesional mempersiapkan wawancara kerja dan meningkatkan CV mereka.",
-    url: "https://www.intervyou.me",
-    siteName: "Intervyou",
-    // images: [
-    //   {
-    //     url: ogImage.src,
-    //     width: 1200,
-    //     height: 630,
-    //     alt: "Intervyou",
-    //   },
-    // ],
-    locale: "id_ID",
-    type: "website",
-  },
-  // twitter: {
-  //   card: "summary_large_image",
-  //   site: "@situsanda",
-  //   creator: "@situsanda",
-  //   title: "Intervyou | Platform Wawancara & Analisis CV",
-  //   description:
-  //     "Platform inovatif yang membantu profesional mempersiapkan wawancara kerja dan meningkatkan CV mereka.",
-  //   images: ["https://www.intervyou.me/twitter-image.jpg"],
-  // },
-  robots: {
-    index: true,
-    follow: true,
-  },
-  alternates: {
-    canonical: "https://www.intervyou.me",
-  },
+    "Lingora is a platform to learn English conversation with AI",
 };
 
 export default function RootLayout({
@@ -91,10 +40,12 @@ export default function RootLayout({
         data-client-key={process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY}
       ></Script>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-primary`}
+      >
         <main>
+          <Toaster />
           <ReactQueryProvider>
-          {children}
+            <NextAuthProvider>{children}</NextAuthProvider>
           </ReactQueryProvider>
         </main>
       </body>
